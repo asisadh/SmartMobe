512df4c56e2440f5889aa4c8af193128a46409e4
Aashish Adhikari <asis.adh@gmail.com>
2019-04-10 16:22:00 +0545

    Listing Protocols version 1

--- SmartMobe/ListingViewController/Protocol/ListingViewProtocols.swift
+++ SmartMobe/ListingViewController/Protocol/ListingViewProtocols.swift
@@ -21,26 +21,43 @@ protocol ListingViewPresenterProtocol: class{
     var view: ListingViewProtocol? {get set}
     var interactor: ListingViewInteractorInputProtocol? {get set}
     var wireFrame: ListingViewWireFrameProtocol? {get set}
+    
+    func viewDidLoad()
+    
+    #warning("change the string to correct model")
+    func showListDetail(list: String)
 }
 
 protocol ListingViewInteractorInputProtocol: class{
+    var presenter: ListingViewInteractorOutputProtocol? {get set}
+    var localDataManager: ListingViewLocalDataManagerProtocol? {get set}
+    var remoteDataManager: ListingViewRemoteDataManagerInputProtocol? {get set}
     
+    func retriveList()
 }
 
 protocol ListingViewInteractorOutputProtocol: class{
     
+    #warning("change the string to correct model")
+    func didRetriveList(list: String)
+    func onError(message: String)
 }
 
 protocol ListingViewRemoteDataManagerInputProtocol: class{
+    var requestHandler: ListingViewRemoteDataManagerOutputProtocol? {get set}
     
+    func retriveList()
 }
 
 protocol ListingViewRemoteDataManagerOutputProtocol: class{
-    
+    func onListRetrieved(list: String)
+    func onError(message: String)
 }
 
 protocol ListingViewLocalDataManagerProtocol: class{
-    
+    #warning("change the string to correct model")
+    func retriveList() throws -> String
+    func saveListItem() throws
 }
 
 protocol ListingViewWireFrameProtocol: class{
