57b8b8f133f9f9ad00e9e423e3a3f7f7680842cf
Aashish Adhikari <asis.adh@gmail.com>
2019-04-11 09:42:12 +0545

    Protocols for Detail View

---
+++ SmartMobe/DetailViewController/DataManager/DetailViewRemoteDataManager.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewRemoteDataManager.swift
+//  SmartMobe
+//
+//  Created by Aashish Adhikari on 4/11/19.
+//  Copyright © 2019 Smart Mobe. All rights reserved.
+//
+
+import Foundation
+
+class DetailViewRemoteDataManager: DetailViewRemoteDataManagerInputProtocol{
+    
+    var requestHandler: DetailViewRemoteDataManagerOutputProtocol?
+    
+    func retriveDetail(imageId id: Int) {
+        
+    }
+}
---
+++ SmartMobe/DetailViewController/Protocol/DetailViewProtocols.swift
@@ -0,0 +1,57 @@
+//
+//  DetailViewProtocols.swift
+//  SmartMobe
+//
+//  Created by Aashish Adhikari on 4/11/19.
+//  Copyright © 2019 Smart Mobe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol DetailViewProtocol: class{
+    var presenter: DetailViewPresenterProtocol? { get set}
+    
+    func showLoading()
+    func hideLoading()
+    func showDetail()
+}
+
+protocol DetailViewPresenterProtocol: class{
+    var view: DetailViewProtocol? {get set}
+    var wireFrame: DetailViewWireFrameProtocol? {get set}
+    var interactor: DetailViewInteractorInputProtocol? {get set}
+    
+    var image: Image {get}
+    
+    func viewDidLoad()
+    func showError(message: String)
+}
+
+protocol DetailViewInteractorInputProtocol: class{
+    var presenter: DetailViewInteractorOutputProtocol? { get set }
+    var dataManager: DetailViewRemoteDataManagerInputProtocol? { get set }
+    
+    func fetchDetail(image: Image)
+}
+
+protocol DetailViewInteractorOutputProtocol: class{
+    func didFetchDetain(image: Image)
+    func onError(message: String)
+}
+
+protocol DetailViewRemoteDataManagerInputProtocol: class{
+    var requestHandler: DetailViewRemoteDataManagerOutputProtocol? {get set}
+    func retriveDetail(imageId id: Int)
+}
+
+protocol DetailViewRemoteDataManagerOutputProtocol: class{
+    func onDetailReterived(image: Image)
+    func onError(message: String)
+}
+
+protocol DetailViewWireFrameProtocol: class{
+    static func createDetailViewModule() -> UIViewController
+    
+    func presentErrorScreen(from view: DetailViewProtocol, errorMessage message: String)
+}
